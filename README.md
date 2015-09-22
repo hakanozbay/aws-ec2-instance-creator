83403526f5b47c42bb500a99ea352b70d43efac8
Hakan Ozbay <hakan.ozbay@gmail.com>
2015-09-22 22:51:48 +0100

    update README
     add the aws folder and Ec2InstanceLauncher.py file

--- README.md
+++ README.md
@@ -1,1 +1,175 @@
 # aws-ec2-instance-creator
+A Python 3 command line script with arguments to create AWS EC2 Instances in your account
+
+# Introduction
+
+This article is a guide on using the EC2 Instance Launcher Python script to create AWS instances. 
+
+# Pre-requisites
+
+There is some setup work to be done before being able to use the script. You may refer to the [getting started][] guide, but specifics are covered below:
+
+1) You must install Python 3
+
+2) You must install the ```boto``` Python package. You can do this by executing the following command:
+
+```
+pip3 install boto
+```
+
+3) You must configure boto, by creating a configuration file. The configuration must contain the following:
+
+```
+[Credentials]
+aws_access_key_id = YOURACCESSKEY
+aws_secret_access_key = YOURSECRETKEY
+```
+
+Your ```access_key_id``` and ```aws_secret_access_key``` are provided to you when you first create your account. You cannot retrieve the values again, but can regenerate them in the AWS console.
+
+On Mac/Unix computers, the boto config file must be created in the the file ```~/.boto```
+
+On Windows computers, create any file but set a user environment variable named ```BOTO_CONFIG``` to the full path of that file.
+
+You can read up further about [boto config][]
+
+# Using the script
+
+When you clone the repository you will see a file entitled ```Ec2InstanceLauncher.py``` in the ```aws``` folder.
+
+You can see how to use this script by running the script with the ```-h``` or ```--help``` flag to understand its usage
+
+```
+$ python3 Ec2InstanceLauncher.py -h
+usage: Ec2InstanceLauncher.py [-h] [-r REGION] [-i IMAGE_ID]
+                              [-t INSTANCE_TYPE] [-d DISK_SIZE]
+                              [-v VOLUME_DIRECTORY]
+                              keyname security_group_name
+
+Create an AWS instance with the specified key name and security groups.
+
+positional arguments:
+  keyname               The key name to use as created in AWS console
+  security_group_name   The security group name to use as created in AWS
+                        console
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -r REGION, --region REGION
+                        Specify the global region to create instance in
+                        (default: eu-west-1)
+  -i IMAGE_ID, --image-id IMAGE_ID
+                        Specify the image ID to create instance from (default:
+                        ami-5f2f5528)
+  -t INSTANCE_TYPE, --instance-type INSTANCE_TYPE
+                        Specify the instance type to create instance from
+                        (default: t2.micro)
+  -d DISK_SIZE, --disk-size DISK_SIZE
+                        Specify the disk size in GB (default: 30)
+  -v VOLUME_DIRECTORY, --volume-directory VOLUME_DIRECTORY
+                        Specify the directory to create volume mount in
+                        (default: /dev/xvda)
+
+```
+
+There are 2 mandatory arguments you must provide: ```keyname``` and ```security_group_name```. the ```keyname``` is the AWS key you would like to use to create the ssh key access of the primary account for the instances to be created. the ```security_group_name``` is the AWS security group that you would like to use to apply security settings on the instance to be created.
+
+There are several optional arguments you can provide aswell. These optional arguments have default values set that will be used unless you specify other values for them. Currently the opitonal values are set to create a CoreOS 681.2.0 server with 30 GB of disk space in volume mount folder /dev/xda. The instance type is the t2.micro, creating this instance in Ireland (eu-west-1).   
+(Note: If you do want to proceed with CoreOS instance creation, your security group must have ports 4001 and 7001 open)
+
+You can now run the script to create an instance, which will output information on the console as it executes:
+
+```
+
+$ python3 Ec2InstanceLauncher.py hakanozbay-key-pair-ireland hakanozbay_SG_euireland
+Attempting to create an AWS Instance using the following parameters:
+{'disk_size': 30,
+ 'image_id': 'ami-5f2f5528',
+ 'instance_type': 't2.micro',
+ 'keyname': 'hakanozbay-key-pair-ireland',
+ 'region': 'eu-west-1',
+ 'security_group_name': 'hakanozbay_SG_euireland',
+ 'volume_directory': '/dev/xvda'}
+====================
+Attempting to conenct to region: eu-west-1
+Connection to region eu-west-1 made succesfully!
+====================
+Creating block device volume size 30 GB of type gp2 in volume directory /dev/xvda
+Block device created successfully!
+====================
+Attempting to create and run instance using the following details -
+Image ID : ami-5f2f5528, Key name: hakanozbay-key-pair-ireland, Security group: hakanozbay_SG_euireland, Instance type: t2.micro, User data: #cloud-config
+
+coreos:
+  etcd:
+    discovery: https://discovery.etcd.io/abcdef
+    addr: $private_ipv4:4001
+    peer-addr: $private_ipv4:7001
+  units:
+    - name: etcd.service
+      command: start
+    - name: fleet.service
+      command: start
+Reservation created succesfully! Details - Reservation ID: r-1ec482b4, Owner ID: 824654475944, Security Groups: [], Instances: [Instance:i-570e9df6]
+====================
+Getting created instance from reservation...
+Instance details:
+{'_in_monitoring_element': False,
+ '_placement': eu-west-1b,
+ '_previous_state': None,
+ '_state': pending(0),
+ 'ami_launch_index': '0',
+ 'architecture': 'x86_64',
+ 'block_device_mapping': {},
+ 'client_token': '',
+ 'connection': EC2Connection:ec2.eu-west-1.amazonaws.com,
+ 'dns_name': '',
+ 'ebs_optimized': False,
+ 'eventsSet': None,
+ 'group_name': None,
+ 'groups': [<boto.ec2.group.Group object at 0x1032d8160>],
+ 'hypervisor': 'xen',
+ 'id': 'i-570e9df6',
+ 'image_id': 'ami-5f2f5528',
+ 'instance_profile': None,
+ 'instance_type': 't2.micro',
+ 'interfaces': [NetworkInterface:eni-96d858df],
+ 'ip_address': None,
+ 'item': '\n        ',
+ 'kernel': None,
+ 'key_name': 'hakanozbay-key-pair-ireland',
+ 'launch_time': '2015-08-19T20:16:18.000Z',
+ 'monitored': False,
+ 'monitoring': '\n            ',
+ 'monitoring_state': 'disabled',
+ 'persistent': False,
+ 'platform': None,
+ 'private_dns_name': 'ip-172-31-42-223.eu-west-1.compute.internal',
+ 'private_ip_address': '172.31.42.223',
+ 'product_codes': [],
+ 'public_dns_name': '',
+ 'ramdisk': None,
+ 'reason': '',
+ 'region': RegionInfo:eu-west-1,
+ 'requester_id': None,
+ 'root_device_name': '/dev/xvda',
+ 'root_device_type': 'ebs',
+ 'sourceDestCheck': 'true',
+ 'spot_instance_request_id': None,
+ 'state_reason': {'code': 'pending', 'message': 'pending'},
+ 'subnet_id': 'subnet-2a62d15d',
+ 'tags': {},
+ 'virtualization_type': 'hvm',
+ 'vpc_id': 'vpc-53f62b36'}
+====================
+Instance created and launched successfully!
+
+``` 
+
+After this is done, you can check on the AWS console to verify that the volume and instance has been created.
+(Note: for CoreOS servers, the service discovery URL is applicable for 10 servers in the cluster)
+
+[boto config]: http://boto.readthedocs.org/en/latest/boto_config_tut.html
+[getting started]: http://boto.readthedocs.org/en/latest/getting_started.html
+
+
---
+++ aws/Ec2InstanceLauncher.py
@@ -0,0 +1,253 @@
+import boto.ec2
+import argparse
+from pprint import pprint
+from time import sleep
+
+DEFAULT_REGION = "eu-west-1" # Ireland
+DEFAULT_AMI_IMAGE_ID = "ami-5f2f5528" # CoreOS-stable-681.2.0-hvm
+DEFAULT_INSTANCE_TYPE= "t2.micro"
+DEFAULT_DISK_SIZE_GB= 30
+DEFAULT_VOLUME_DIRECTORY = "/dev/xvda" # CoreOS device mount location
+
+KEY_NAME = None
+SECURITY_GROUPS_NAMES = None
+REGION = None
+AMI_IMAGE_ID = None
+INSTANCE_TYPE = None
+DISK_SIZE_GB = None
+VOLUME_DIRECTORY = None
+NAME = None
+ROLE_PROFILE = None
+
+def parse_commandline_arguments():
+
+	global KEY_NAME
+	global SECURITY_GROUPS_NAMES
+	global REGION
+	global AMI_IMAGE_ID
+	global INSTANCE_TYPE
+	global DISK_SIZE_GB
+	global VOLUME_DIRECTORY
+	global NAME
+	global ROLE_PROFILE
+	
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,description='Create an AWS instance with the specified key name and security groups.')
+	parser.add_argument("keyname", type=str, help="The key name to use as created in AWS console")
+	parser.add_argument("security_group_name",type=str, help="The security group name to use as created in AWS console")
+	parser.add_argument("-r","--region", dest="region", type=str, default=DEFAULT_REGION, help="Specify the global region to create instance in")
+	parser.add_argument("-i","--image-id", dest="image_id", type=str, default=DEFAULT_AMI_IMAGE_ID, help="Specify the image ID to create instance from")
+	parser.add_argument("-t","--instance-type", dest="instance_type", type=str, default=DEFAULT_INSTANCE_TYPE, help="Specify the instance type to create instance from")
+	parser.add_argument("-d","--disk-size", dest="disk_size", type=int, default=DEFAULT_DISK_SIZE_GB, help="Specify the disk size in GB")
+	parser.add_argument("-v","--volume-directory", dest="volume_directory", type=str, default=DEFAULT_VOLUME_DIRECTORY, help="Specify the directory to create volume mount in")
+	parser.add_argument("-n","--name", dest="name_tag", type=str, help="Specify the name of the instance and volume to be created")
+	parser.add_argument("-p","--role-profile", dest="profile", type=str, help="Specify the IAM role profile to apply to this instance")
+	
+	args = parser.parse_args()
+
+	print ("Attempting to create an AWS Instance using the following parameters:")
+	pprint(vars(args))
+
+	KEY_NAME = args.keyname
+	SECURITY_GROUPS_NAMES = args.security_group_name
+	REGION = args.region
+	AMI_IMAGE_ID = args.image_id
+	INSTANCE_TYPE = args.instance_type
+	DISK_SIZE_GB = args.disk_size
+	VOLUME_DIRECTORY = args.volume_directory
+
+	if args.name_tag:
+		NAME = args.name_tag
+
+	if args.profile:
+		ROLE_PROFILE = args.profile
+
+
+def launch_instance():
+
+	connection = create_connection_to_region()
+	reservation = create_instance(connection)
+	instance = get_instance_from_reservation(reservation)
+	address = create_elastic_ip_address(connection, instance.id)
+	assign_elastic_ip_address(connection, instance.id, address)
+
+	if NAME is not None:
+		assign_name_tag_to_instance(connection, instance.id)
+		assign_name_tag_to_volume(connection, instance.id)
+
+	instance_details = get_instance_details(connection, instance.id)
+
+	print ("====================")
+	print ("Instance details:")
+	pprint(instance_details.__dict__)
+	
+	
+def create_connection_to_region():
+
+	print ("====================")
+	print ("Attempting to connect to region: %s" % REGION)
+	
+	connection = boto.ec2.connect_to_region(REGION)
+
+	if connection is None:
+		raise ConnectionError("Could not connect to region! Check your connection or region") 
+	
+	print ("Connection to region %s made succesfully!" % REGION)
+	return connection
+
+
+def create_instance(connection):
+
+	user_data = get_user_data()
+	block_device_map = create_block_device_mapping()
+	print ("====================")
+	print ("Attempting to create and run instance using the following details -")
+	print ("Image ID : %s, Key name: %s, Security group: %s, Instance type: %s, User data: %s" %(AMI_IMAGE_ID, KEY_NAME, SECURITY_GROUPS_NAMES, INSTANCE_TYPE, user_data))
+	
+	reservation = connection.run_instances (AMI_IMAGE_ID,
+		key_name = KEY_NAME,
+		security_groups=[SECURITY_GROUPS_NAMES],
+		user_data=user_data,
+		instance_type=INSTANCE_TYPE,
+		block_device_map = block_device_map,
+		instance_profile_name=ROLE_PROFILE)
+
+	if reservation is None:
+		raise Exception( "Reservation not created! Try again")
+
+	print ("Reservation created succesfully! Details - Reservation ID: %s, Owner ID: %s, Security Groups: %s, Instances: %s" % (reservation.id, reservation.owner_id, reservation.groups, reservation.instances))
+	
+	if len(reservation.instances) == 0:
+		raise Exception ("There is no created instance from this attempt!")
+	
+	return reservation
+
+
+def get_user_data():
+
+	cloud_config ="""#cloud-config
+
+coreos:
+  etcd:
+    discovery: https://discovery.etcd.io/<write_your_own_id>
+    addr: $private_ipv4:4001
+    peer-addr: $private_ipv4:7001
+  units:
+    - name: etcd.service
+      command: start
+    - name: fleet.service
+      command: start"""
+
+	return cloud_config
+
+
+def create_block_device_mapping():
+
+	print ("====================")
+	print ("Creating block device volume size %s GB of type %s in volume directory %s" % (DISK_SIZE_GB, "gp2", VOLUME_DIRECTORY))
+
+	block_device_type = boto.ec2.blockdevicemapping.BlockDeviceType(size=DISK_SIZE_GB,volume_type="gp2")
+
+	block_device_mapping = boto.ec2.blockdevicemapping.BlockDeviceMapping()
+	block_device_mapping [VOLUME_DIRECTORY] = block_device_type
+
+	print ("Block device created successfully!")
+
+	return block_device_mapping
+
+def get_instance_from_reservation(reservation):
+
+	print ("====================")
+	print ("Getting created instance from reservation...")
+	
+	instance = reservation.instances[0]
+
+	if instance is None:
+		raise Exception ("Instance does not exist! Check the AWS console to verify creation or try again")
+
+	print ("Instance created and launched successfully!")
+
+	return instance
+
+def create_elastic_ip_address(connection,instance_id):
+
+	print ("====================")
+	print ("Creating an Elastic IP...")
+
+	address = connection.allocate_address()
+
+	if address is None:
+		print("Elastic IP address creation failed! Check the AWS console to verify and perform action manually.")
+		
+	else:
+		print("Elastic IP address details:")
+		pprint(address.__dict__)
+		print("Elastic IP address created successfully!")
+
+	return address
+
+	
+def assign_elastic_ip_address(connection,instance_id, address):
+
+	print ("====================")
+	
+	if address is None:
+		print("Elastic IP does not exist. Skipping assingment step...")
+
+	else:
+		print ("Assigning an Elastic IP to instance %s" % instance_id)
+		
+		wait_for_instance_to_be_running(connection, instance_id)
+		successful_association= connection.associate_address(instance_id=instance_id, allocation_id=address.allocation_id)
+
+		if successful_association:
+			print("Elastic IP address assignment successful!")
+
+		else:
+			print("Elastic IP address assignment failed! Check the AWS console to verify and perform action manually.")
+
+def wait_for_instance_to_be_running(connection,instance_id):
+
+	instance_state_code = None
+	while instance_state_code is not 16:
+		print ("Waiting for instance state to be 'running'")
+		sleep(5)
+		instance = get_instance_details(connection,instance_id)
+		instance_state_code = instance.state_code
+
+def assign_name_tag_to_instance(connection, instance_id):
+
+	print ("====================")
+	print("Assigning name tag %s to instance %s" %(NAME,instance_id))
+
+	connection.create_tags([instance_id],{"Name": NAME})
+	print ("Name tag applied to instance successfully!")
+
+def assign_name_tag_to_volume(connection, instance_id):
+
+	print ("====================")
+	volume_name_tag = NAME + "_volume"
+	volumes = connection.get_all_volumes(filters={'attachment.instance-id': instance_id})
+
+	if len(volumes) == 0:
+		print("There are no volumes associate with this instance! Name tag cannot be assigned. Check the AWS console to verify and perform action manually")
+
+	else:
+		for volume in volumes:
+			print("Assigning name tag %s to volume %s" % (volume_name_tag,volume.id))
+			connection.create_tags([volume.id],{"Name": volume_name_tag})
+			print ("Name tag applied to volume successfully!")
+
+def get_instance_details(connection, instance_id):
+
+	instances = connection.get_only_instances(instance_ids=[instance_id])
+	instance = instances[0]
+	return instance
+
+
+if __name__ == "__main__":
+
+	try:
+		parse_commandline_arguments()
+		launch_instance()
+	except Exception as error:
+		print (str(error))
